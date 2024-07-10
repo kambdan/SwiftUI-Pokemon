e678682bfe0aae7cdcfe5bc6b0794f4c57a255e0
kambdan <juancambi1319@gmail.com>
2024-07-09 20:40:52 -0500

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Juan Cambizaca on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
